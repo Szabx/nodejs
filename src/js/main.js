2c1141d4e537b2ab1dae706f09b0b6957b8e9aa3
Szabolcs Gergely <szabx90@gmail.com>
2016-03-13 15:54:54 +0200

    ssh test

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 // Get users from json
 jQuery.getJSON('users', function(users) {
 	/*optional stuff to do after success */
-	console.log(users);
+	console.log('users', users);
 	// Write to paragraph
 	// $("body").append("<p>" + JSON.stringify(users) + "</p>");
 });
